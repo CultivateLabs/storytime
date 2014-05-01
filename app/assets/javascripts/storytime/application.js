bdbe056344506ca7f3de7a0bfc23b11a64400366
Ben Roesch <bcroesch@gmail.com>
2014-05-01 13:14:07 -0500

    Add bootstrap

--- app/assets/javascripts/storytime/application.js
+++ app/assets/javascripts/storytime/application.js
@@ -10,4 +10,5 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require_tree .
--- lib/storytime/engine.rb
+++ lib/storytime/engine.rb
@@ -1,4 +1,5 @@
 require 'devise'
+require 'bootstrap-sass'
 
 module Storytime
   class Engine < ::Rails::Engine
