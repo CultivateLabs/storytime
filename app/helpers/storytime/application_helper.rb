a6c1ac6fd94d30a421256b2d50359384cd77570a
Ben Roesch <bcroesch@gmail.com>
2014-05-07 20:33:30 -0500

    Add new posts button on dashboard/posts

--- app/helpers/storytime/application_helper.rb
+++ app/helpers/storytime/application_helper.rb
@@ -1,8 +1,8 @@
 module Storytime
   module ApplicationHelper
 
-    def active_nav_item_class(path)
-      'class="active"'.html_safe if path == request.path
+    def active_nav_item_class(controller)
+      'class="active"'.html_safe if controller == params[:controller].split("/").last
     end
 
   end
