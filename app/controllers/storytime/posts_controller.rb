43d5e646f5332a78213da6eb9b0753399a5e145c
David Van Der Beek <earlynovrock@gmail.com>
2014-05-14 16:46:11 -0500

    order posts by created_at

--- app/controllers/storytime/posts_controller.rb
+++ app/controllers/storytime/posts_controller.rb
@@ -8,7 +8,7 @@ module Storytime
       else
         Post.all
       end
-      @posts = @posts.page(params[:page]).per(10)
+      @posts = @posts.order(created_at: :desc).page(params[:page]).per(10)
     end
 
     def show
