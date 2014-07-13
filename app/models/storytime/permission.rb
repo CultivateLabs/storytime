5bc49447bbb45670e8b84794e24e2a22e8d162e7
David Van Der Beek <earlynovrock@gmail.com>
2014-07-13 14:33:45 -0500

    clean up seed formatting a little

--- app/models/storytime/permission.rb
+++ app/models/storytime/permission.rb
@@ -6,12 +6,12 @@ module Storytime
     def self.seed
       writer = Role.find_by(name: "writer")
       editor = Role.find_by(name: "editor")
-      admin = Role.find_by(name: "admin")
+      admin  = Role.find_by(name: "admin")
 
-      publish_own = Action.find_by(guid: "5030ed")
+      publish_own   = Action.find_by(guid: "5030ed")
       manage_others = Action.find_by(guid: "d8a1b1")
-      manage_site = Action.find_by(guid: "47342a")
-      manage_users = Action.find_by(guid: "1f7d47")
+      manage_site   = Action.find_by(guid: "47342a")
+      manage_users  = Action.find_by(guid: "1f7d47")
 
       create(role: writer, action: publish_own)
       create(role: editor, action: publish_own)
