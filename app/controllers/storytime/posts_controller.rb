006a02272e7e86f428ef80a6bc21614a322a70ef
Ben Roesch <bcroesch@gmail.com>
2014-07-13 20:52:14 -0500

    Finish adding atom feed

--- app/controllers/storytime/posts_controller.rb
+++ app/controllers/storytime/posts_controller.rb
@@ -4,13 +4,17 @@ module Storytime
   class PostsController < ApplicationController
     before_action :ensure_site, unless: ->{ params[:controller] == "storytime/dashboard/sites" }
     def index
-      binding.pry
       @posts = if params[:tag]
         Post.tagged_with(params[:tag])
       else
         Post.all
       end
       @posts = @posts.published.order(created_at: :desc).page(params[:page]).per(10)
+
+      respond_to do |format|
+        format.atom
+        format.html
+      end
     end
 
     def show
