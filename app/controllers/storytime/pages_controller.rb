3e419c72f632fe55bfdd4dcf9d96bd6c84e2d6f2
Ben Roesch <bcroesch@gmail.com>
2014-07-28 18:12:16 -0500

    Allow overriding of single page template by host app

--- app/controllers/storytime/pages_controller.rb
+++ app/controllers/storytime/pages_controller.rb
@@ -11,6 +11,9 @@ module Storytime
       if request.path != page_path(@page)
         return redirect_to @page, :status => :moved_permanently
       end
+
+      #allow overriding in the host app
+      render @page.slug if lookup_context.template_exists?("storytime/pages/#{@page.slug}")
     end
   end
 end
