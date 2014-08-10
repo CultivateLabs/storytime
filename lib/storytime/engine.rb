f134de102e54ee3cd6e3439d45a7ce9497ea768b
Ben Roesch <bcroesch@gmail.com>
2014-08-10 14:00:09 -0500

    Move carrierwave config so that Storytime is configured first

--- lib/storytime/engine.rb
+++ lib/storytime/engine.rb
@@ -25,5 +25,26 @@ module Storytime
         include Storytime::ControllerHelpers
       end
     end
+
+    # putting this here rather than config/initializers so that Storytime is configured before getting here
+    initializer "storytime.configure_carrierwave" do
+      CarrierWave.configure do |config|
+        if Storytime.media_storage == :s3
+          config.storage = :fog
+          config.fog_credentials = {
+            :provider               => 'AWS',
+            :aws_access_key_id      => ENV['STORYTIME_AWS_ACCESS_KEY_ID'],
+            :aws_secret_access_key  => ENV['STORYTIME_AWS_SECRET_KEY']
+          }
+          config.fog_directory  = Storytime.s3_bucket
+          config.fog_public     = true
+          config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+        else
+          config.storage = :file
+        end
+        
+        config.enable_processing = !Rails.env.test?
+      end
+    end
   end
 end
