6b8291defb38b98c718a0a6bb2b6cda3d403fdce
Ben Roesch <bcroesch@gmail.com>
2014-08-29 15:37:10 -0500

    Namespace application partials to prevent conflicting with host app

--- app/helpers/storytime/application_helper.rb
+++ app/helpers/storytime/application_helper.rb
@@ -39,5 +39,31 @@ module Storytime
         yield(tag, classes[index.round])
       end
     end
+
+
+    def method_missing method, *args, &block
+      if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
+        if main_app.respond_to?(method)
+          main_app.send(method, *args)
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to?(method)
+      if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
+        if main_app.respond_to?(method)
+          true
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+
   end
 end
