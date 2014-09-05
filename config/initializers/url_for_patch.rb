13ed915f7b3fa05860c277f19ffa2351ae2faeea
Ben Roesch <bcroesch@gmail.com>
2014-09-05 13:22:23 -0500

    Zero pad dates in post urls

--- config/initializers/url_for_patch.rb
+++ config/initializers/url_for_patch.rb
@@ -19,14 +19,14 @@ module ActionDispatch
               options[:component_1] = "posts"
             when "day_and_name" 
               date = post.created_at.to_date
-              options[:component_1] = date.year
-              options[:component_2] = date.month
-              options[:component_3] = date.day
+              options[:component_1] = date.strftime("%Y") # 4 digit year
+              options[:component_2] = date.strftime("%m") # 2 digit month
+              options[:component_3] = date.strftime("%d") # 2 digit day
               options[:id] = post
             when "month_and_name"
               date = post.created_at.to_date
-              options[:component_1] = date.year
-              options[:component_2] = date.month
+              options[:component_1] = date.strftime("%Y") # 4 digit year
+              options[:component_2] = date.strftime("%m") # 2 digit month
               options[:id] = post
             when "post_name"
               options[:component_1] = nil
