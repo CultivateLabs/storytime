9a230aaaff71702925b00185b7da5a07cb912079
eanlain <brandon@ourlabel.com>
2014-11-05 20:25:02 -0600

    Fix #46
    Rename select migrations to prevent name collision

--- db/migrate/20140521191048_add_storytime_role_id_to_users.rb
+++ db/migrate/20140521191048_add_storytime_role_id_to_users.rb
@@ -1,4 +1,4 @@
-class AddRoleIdToUsers < ActiveRecord::Migration
+class AddStorytimeRoleIdToUsers < ActiveRecord::Migration
   def change
     add_column Storytime.user_class.table_name.to_sym, :storytime_role_id, :integer
     add_index Storytime.user_class.table_name.to_sym, :storytime_role_id
