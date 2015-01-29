0690a4efce1378a5a873eb21b684c84b123db5c9
eanlain <brandon@ourlabel.com>
2015-01-29 11:22:40 -0600

    Fix syntax in subscription migration

--- db/migrate/20141111164439_create_storytime_subscriptions.rb
+++ db/migrate/20141111164439_create_storytime_subscriptions.rb
@@ -3,7 +3,7 @@ class CreateStorytimeSubscriptions < ActiveRecord::Migration
     create_table :storytime_subscriptions do |t|
       t.string :email
       t.boolean :subscribed, default: true
-      t.token :string, index: true
+      t.string :token, index: true
 
       t.timestamps
     end
