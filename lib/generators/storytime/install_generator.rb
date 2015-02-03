921128319b7da8bc4bc29990197cac41628bf13f
eanlain <brandon@ourlabel.com>
2015-02-02 18:45:13 -0600

    Use instance variables within generator template

--- lib/generators/storytime/install_generator.rb
+++ lib/generators/storytime/install_generator.rb
@@ -7,6 +7,24 @@ module Storytime
 
       desc "Creates a Storytime initializer for your application."
 
+      def set_local_assigns
+        @layout = 'application'
+        @user_class = 'User'
+        @dashboard_namespace_path = '/storytime'
+        @home_page_path = '/'
+        @post_types = ['CustomPostType']
+        @post_title_character_limit = 255
+        @post_excerpt_character_limit = 500
+        @whitelisted_html_tags = '%w(p blockquote pre h1 h2 h3 h4 h5 h6 span ul li ol table tbody td br a img iframe hr)'
+        @disqus_forum_shortname = ''
+        @email_regexp = '/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/'
+        @search_adapter = "''"
+        @enable_file_upload = true
+        @s3_bucket = 'my-s3-bucket'
+        @prod_media_storage = ':s3'
+        @dev_media_storage = ':file'
+      end
+
       def copy_initializer
         template "storytime.rb", "config/initializers/storytime.rb"
       end
