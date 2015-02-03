833bd80131aa64129d488f890d9c80324bd405da
eanlain <brandon@ourlabel.com>
2015-02-02 18:45:58 -0600

    Use autoload for search adapters

--- lib/storytime.rb
+++ lib/storytime.rb
@@ -1,10 +1,12 @@
-require "storytime/engine"
-require "storytime/mysql_search_adapter"
-require "storytime/mysql_fulltext_search_adapter"
-require "storytime/postgres_search_adapter"
-require "storytime/sqlite3_search_adapter"
+require 'rails'
+require 'storytime/engine'
 
 module Storytime
+  autoload :MysqlSearchAdapter,         'storytime/mysql_search_adapter'
+  autoload :MysqlFulltextSearchAdapter, 'storytime/mysql_fulltext_search_adapter'
+  autoload :PostgresSearchAdapter,      'storytime/postgres_search_adapter'
+  autoload :Sqlite3SearchAdapter,       'storytime/sqlite3_search_adapter'
+
   # Model to use for Storytime users.
   mattr_accessor :user_class
   @@user_class = 'User'
@@ -12,12 +14,12 @@ module Storytime
   # Path of Storytime's dashboard, relative to
   # Storytime's mount point within the host app.
   mattr_accessor :dashboard_namespace_path
-  @@dashboard_namespace_path = "/storytime"
+  @@dashboard_namespace_path = '/storytime'
 
   # Path of Storytime's home page, relative to
   # Storytime's mount point within the host app.
   mattr_accessor :home_page_path
-  @@home_page_path = "/"
+  @@home_page_path = '/'
 
   # Enable file uploads through Carrierwave.
   mattr_accessor :enable_file_upload
@@ -33,14 +35,14 @@ module Storytime
 
   # Array of tags to allow from the Summernote WYSIWYG
   # Editor when editing Posts and custom post types.
-  # An empty array, "", or nil setting will permit all tags.
+  # An empty array, '', or nil setting will permit all tags.
   mattr_accessor :whitelisted_post_html_tags
   @@whitelisted_post_html_tags = []
 
   # Enable Disqus comments using your forum's shortname,
   # the unique identifier for your website as registered on Disqus.
   mattr_accessor :disqus_forum_shortname
-  @@disqus_forum_shortname = ""
+  @@disqus_forum_shortname = ''
   
   # Email regex used to validate email format validity for subscriptions.
   mattr_accessor :email_regexp
@@ -48,14 +50,14 @@ module Storytime
 
   # Email address of the sender of subscription emails.
   mattr_accessor :subscription_email_from
-  @@subscription_email_from = "no-reply@example.com"
+  @@subscription_email_from = 'no-reply@example.com'
 
   # Search adapter to use for searching through Storytime Posts or
   # Post subclasses. Options for the search adapter include:
   # Storytime::PostgresSearchAdapter, Storytime::MysqlSearchAdapter,
   # Storytime::MysqlFulltextSearchAdapter, Storytime::Sqlite3SearchAdapter
   mattr_accessor :search_adapter
-  @@search_adapter = Storytime::PostgresSearchAdapter
+  @@search_adapter = nil
 
   mattr_accessor :admin_models
   @@admin_models = []
@@ -83,27 +85,27 @@ module Storytime
 
     def snippet(name)
       snippet = Storytime::Snippet.find_by(name: name)
-      snippet.nil? ? "" : snippet.content.html_safe
+      snippet.nil? ? '' : snippet.content.html_safe
     end
 
     def home_page_route_options
-      site = Storytime::Site.first
+      site = Storytime::Site.first if ActiveRecord::Base.connection.table_exists? 'storytime_sites'
 
       if site
-        if site.root_page_content == "page"
-          { to: "pages#show", as: :storytime_root_post }
+        if site.root_page_content == 'page'
+          { to: 'pages#show', as: :storytime_root_post }
         else
-          { to: "posts#index", as: :storytime_root_post }
+          { to: 'posts#index', as: :storytime_root_post }
         end
       else
-        { to: "application#setup", as: :storytime_root }
+        { to: 'application#setup', as: :storytime_root }
       end
     end
 
     def post_index_path_options
-      site = Storytime::Site.first
+      site = Storytime::Site.first if ActiveRecord::Base.connection.table_exists? 'storytime_sites'
 
-      if site && site.root_page_content == "posts"
+      if site && site.root_page_content == 'posts'
         { path: Storytime.home_page_path }
       else
         {}
