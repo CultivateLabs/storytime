b87c461b07b9e7c7af4ee84f174109fdd48d53f6
David Van Der Beek <earlynovrock@gmail.com>
2015-02-12 16:27:08 -0600

    update snippet partial and check pundit for editing ability

--- app/helpers/storytime/application_helper.rb
+++ app/helpers/storytime/application_helper.rb
@@ -1,6 +1,10 @@
 module Storytime
   module ApplicationHelper
 
+    def logged_in_storytime_user?
+      user_signed_in? && current_user.respond_to?(:storytime_user?) && current_user.storytime_user?
+    end
+
     def dashboard_nav_site_path(site)
       site.nil? || site.new_record? ? storytime.new_dashboard_site_path : storytime.edit_dashboard_site_path(site)
     end
