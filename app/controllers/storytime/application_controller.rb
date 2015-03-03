dca6d44cb0de9bb24cc86fb838269ca0b44fb891
Ben Roesch <bcroesch@gmail.com>
2015-03-02 20:07:46 -0600

    Fix redirect when user is not authorized

--- app/controllers/storytime/application_controller.rb
+++ app/controllers/storytime/application_controller.rb
@@ -55,6 +55,6 @@ private
   
   def user_not_authorized
     flash[:error] = "You are not authorized to perform this action."
-    redirect_to(request.referrer || storytime_root_path)
+    redirect_to(request.referrer || "/")
   end
 end
