ec56b0fe818d303c41fb85146bbde7fd14394ace
Ben Roesch <bcroesch@gmail.com>
2015-03-09 19:12:41 -0500

    Fix dashboard_controller_specs

--- spec/factories/site_factories.rb
+++ spec/factories/site_factories.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :site, class: Storytime::Site do
     sequence(:title) {|i| "Site Title #{i}"}
     subscription_email_from "notifications@site.com"
-    custom_domain "www.lvh.me"
+    sequence(:custom_domain){|i| "site-#{i}.lvh.me" } 
     post_slug_style 0
   end
 end
