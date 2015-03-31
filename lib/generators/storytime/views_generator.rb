fc4a3bfaa9a40b56677bd22707ecf3d9f9e033cc
Brandon Robins <brandon@ourlabel.com>
2015-03-31 08:47:52 -0500

    Add blogs and snippets folder to view generator

--- lib/generators/storytime/views_generator.rb
+++ lib/generators/storytime/views_generator.rb
@@ -9,7 +9,7 @@ module Storytime
         argument :scope, :required => false, :default => nil,
                          :desc => "The scope to copy views to"
 
-        class_option :views, aliases: "-v", type: :array, desc: "Select specific view directories to generate (application, blog_posts, comments, dashboard, pages, posts, sites, subscription_mailer, subscriptions)"
+        class_option :views, aliases: "-v", type: :array, desc: "Select specific view directories to generate (application, blog_posts, blogs, comments, dashboard, pages, posts, sites, snippets, subscription_mailer, subscriptions)"
 
         public_task :copy_views
       end
@@ -28,10 +28,12 @@ module Storytime
         else
           view_directory :application
           view_directory :blog_posts
+          view_directory :blogs
           view_directory :comments
           view_directory :pages
           view_directory :posts
           view_directory :sites
+          view_directory :snippets
           view_directory :subscription_mailer
           view_directory :subscriptions
         end
