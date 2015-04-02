3b84361473a881cdd2471b4b67c24ac1456e9bff
Brandon Robins <brandon@ourlabel.com>
2015-04-02 11:17:04 -0500

    Remove subdomain from Storytime::Site

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331162329) do
+ActiveRecord::Schema.define(version: 20150402161427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,7 +137,6 @@ ActiveRecord::Schema.define(version: 20150331162329) do
     t.integer  "root_post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "subdomain"
     t.string   "subscription_email_from"
     t.string   "layout"
     t.string   "disqus_forum_shortname"
