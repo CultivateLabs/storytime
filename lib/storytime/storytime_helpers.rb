b10e091b83db06b85cf858a53ddc69734cb7d991
Brandon Robins <brandon@flyoverworks.com>
2015-04-22 14:05:51 -0500

    Fallback to translation file if nil snippet

--- lib/storytime/storytime_helpers.rb
+++ lib/storytime/storytime_helpers.rb
@@ -2,8 +2,8 @@ module Storytime
   module StorytimeHelpers
     def storytime_snippet(name)
       snippet = Storytime::Snippet.find_by(name: name)
-      if snippet.nil?
-        ""
+      if snippet.blank?
+        I18n.t(name).html_safe
       else
         render partial: "storytime/snippets/snippet", locals: {snippet: snippet}
       end
