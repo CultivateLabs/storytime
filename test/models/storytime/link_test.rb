95894f945c10fb3cea61112c886726846b5bb719
David Van Der Beek <earlynovrock@gmail.com>
2015-05-20 14:36:09 -0500

    remove test directory

--- test/models/storytime/link_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Storytime
-  class LinkTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
--- test/models/storytime/navigation_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Storytime
-  class NavigationTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
