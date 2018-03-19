064018634ab1e089cc79da529029b818720f15e0
Ben Roesch <bcroesch@gmail.com>
2018-03-18 19:57:28 -0500

    Make homepage relation optional so we can create a site

--- app/models/storytime/site.rb
+++ app/models/storytime/site.rb
@@ -14,7 +14,7 @@ module Storytime
     has_many :pages, -> { where(type: "Storytime::Page") }, dependent: :destroy
     has_many :blogs, dependent: :destroy
     has_many :navigations, dependent: :destroy
-    belongs_to :homepage, class_name: "Storytime::Post", foreign_key: "root_post_id"
+    has_one :homepage, class_name: "Storytime::Post", foreign_key: "root_post_id", required: false
     belongs_to :creator, class_name: Storytime.user_class.to_s, foreign_key: "user_id"
 
     validates :subscription_email_from, presence: true
