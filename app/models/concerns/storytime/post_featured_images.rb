d02713de34a64a526ed5db6383bfd89c8482e950
Ben Roesch <ben@cultivatelabs.com>
2018-10-06 14:09:19 -0500

    Make post media optional

--- app/models/concerns/storytime/post_featured_images.rb
+++ app/models/concerns/storytime/post_featured_images.rb
@@ -2,7 +2,7 @@ module Storytime::PostFeaturedImages
   extend ActiveSupport::Concern
 
   included do
-    belongs_to :featured_media, class_name: "Storytime::Media"
-    belongs_to :secondary_media, class_name: "Storytime::Media"
+    belongs_to :featured_media, class_name: "Storytime::Media", optional: true
+    belongs_to :secondary_media, class_name: "Storytime::Media", optional: true
   end
 end
